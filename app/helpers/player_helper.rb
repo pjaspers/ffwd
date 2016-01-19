5a03db0e0592828ce21cbaa72c0f4043027e0f3c
pjaspers <piet@10to1.be>
2016-01-19 10:51:45 +0100

    Adding player helper

---
+++ app/helpers/player_helper.rb
@@ -0,0 +1,10 @@
+module PlayerHelper
+  def nice_time(secs)
+    [[60, :s], [60, :m], [24, :h], [1000, :d]].map{ |count, name|
+      if secs > 0
+        secs, n = secs.divmod(count)
+        "#{n.to_i}#{name}"
+      end
+    }.compact.reverse.join('')
+  end
+end
